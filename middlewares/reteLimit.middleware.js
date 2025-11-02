e701d1334a4fa1bc562efb730df1de9ae506233e
geraldat1 <geraldjat18@gmail.com>
2025-11-02 03:56:41 -0500

    reteLimit a 200

--- middlewares/reteLimit.middleware.js
+++ middlewares/reteLimit.middleware.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Middleware de rate limit
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 5 minutos en milisegundos
-  max: 30, // 30 peticiones por cada 5 minutos
+  max: 200, // 200 peticiones por cada 5 minutos
   message: 'Demasiadas solicitudes desde esta IP, por favor intente de nuevo más tarde.',
   headers: true, // Añade información sobre el rate limit en las cabeceras de la respuesta
 });
